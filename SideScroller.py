2bf5da5603364771834268fd77897ad6691fc6d0
Rex <lucas.colas13@gmail.com>
2020-03-21 15:00:55 +0100

    moved hitbox

--- SideScroller.py
+++ SideScroller.py
@@ -37,7 +37,7 @@ class player(object):
         self.jumpCount = 0
         self.runCount = 0
         self.slideUp = False
-        self.hitbox = None
+        self.hitbox = (self.x+ 4,self.y,self.width-24,self.height-10)
 
     def draw(self, win):
         if self.falling:
@@ -99,10 +99,9 @@ class saw(object):
         self.rotateCount = 0
         self.vel = 1.4
         self.passed = False
-        self.hitbox = None
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)
 
     def draw(self,win):
-        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)
         #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
         if self.rotateCount >= 8:
             self.rotateCount = 0
@@ -123,11 +122,10 @@ class spike(saw):
         self.x = x
         self.width = width
         self.height = height
-        self.hitbox = None
+        self.hitbox = (self.x + 10, self.y + 5, self.width - 20, self.height - 5)
 
         self.passed = False
     def draw(self,win):
-        self.hitbox = (self.x + 10, self.y, 28,315)
         #pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
         win.blit(self.img, (self.x,self.y))
 
