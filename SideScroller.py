d4a9bee41fbfe034859b0c678479fb65105faf17
Rex <lucas.colas13@gmail.com>
2020-03-22 12:17:03 +0100

    moved

--- SideScroller.py
+++ SideScroller.py
@@ -14,8 +14,6 @@ win = pygame.display.set_mode((W,H))
 pygame.display.set_caption('Side Scroller')
 
 bg = pygame.image.load(os.path.join('images','bg.png')).convert()
-global bgX
-global bgX2
 
 bgX = 0
 bgX2 = bg.get_width()
