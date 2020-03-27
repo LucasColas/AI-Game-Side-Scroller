5350f4d677524e806ebf357337f7ba82215af122
Rex <lucas.colas13@gmail.com>
2020-03-27 10:11:59 +0100

    inputs

--- SideScroller.py
+++ SideScroller.py
@@ -237,7 +237,7 @@ def main(genomes, config):
             #runner.draw()
             ge[x].fitness += 0.1
 
-            inputs = (runner.x, runner.get_distance(obstacles))
+            inputs = (runner.get_distance(obstacles))
             outputs = nets[x].activate(inputs)
 
             if outputs[0] > 0.5:
