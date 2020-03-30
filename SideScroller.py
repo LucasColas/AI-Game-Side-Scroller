25e64e3ab0cbf89f31d16412d8752ad39ace3408
Rex <lucas.colas13@gmail.com>
2020-03-30 23:14:06 +0200

    deleted an input

--- SideScroller.py
+++ SideScroller.py
@@ -231,7 +231,7 @@ def main(genomes, config):
             #runner.draw()
             ge[x].fitness += 0.1
 
-            inputs = (runner.x, runner.get_distance(obstacles))
+            inputs = (runner.get_distance(obstacles))
             outputs = nets[x].activate(inputs)
 
             x_obs = inputs[0]
