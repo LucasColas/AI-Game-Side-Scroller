97367211c3a54029836f9c5890156f359bd3af93
MrRex <lucas.colas13@gmail.com>
2020-04-13 22:19:14 +0200

    new trial

--- SideScroller.py
+++ SideScroller.py
@@ -274,22 +274,29 @@ def main(genomes, config):
         if add_obstacle:
             score += 1
             increase_fitness = 15
-            for x in obstacles:
-                for y in obstacles:
-                    if math.sqrt((x.x - y.x)**2) > 200 and x != y :
-                        #obstacles.remove(x)
-                        r = random.randrange(0,3)
-                        if r == 0:
+            r = random.randrange(0,3)
+            if r == 0:
+                for x in obstacles:
+                    for y in obstacles:
+                        if math.sqrt((x.x - y.x)**2) > 200 and x != y :
+                            #obstacles.remove(x)
                             obstacles.append(saw(810, 310, 64, 64))
                             obstacles.append(spike(1250, 0, 48, 310))
 
-                        if r == 1:
+            if r == 1:
+                for x in obstacles:
+                    for y in obstacles:
+                        if math.sqrt((x.x - y.x)**2) > 200 and x != y :
+                            #obstacles.remove(x)
                             obstacles.append(spike(810, 0, 48, 310))
                             obstacles.append(saw(1250, 310, 64, 64))
-                        elif r == 2:
+            elif r == 2:
+                for x in obstacles:
+                    for y in obstacles:
+                        if math.sqrt((x.x - y.x)**2) > 200 and x != y :
+                            obstacles.remove(x)
                             obstacles.append(spike(1010, 0, 48, 310))
                             obstacles.append(saw(1250, 310, 64, 64))
-
             add_obstacle = False
             for g in ge:
                 g.fitness += increase_fitness
